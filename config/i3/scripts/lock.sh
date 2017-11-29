22d32af15360d38f208b16156e22ecd9ed15e1e7
Mark Medum Bundgaard <mmedum@gmail.com>
2017-11-29 21:11:56 +0100

    updates to fadein effects by using compton and new lock script to make everything pretty

--- config/i3/scripts/lock.sh
+++ config/i3/scripts/lock.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env sh
-TMPBG=/tmp/screen.png
 LOCK=/home/mark/.config/i3/scripts/lock.png
-RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
 
-param=(--textcolor=ffffff00 --insidecolor=faddc500 --ringcolor=fafafa00 \
-    --linecolor=2d283e00 --keyhlcolor=fabb5caa --ringvercolor=fadd5caa \
-    --separatorcolor=22222200 \
-    --insidevercolor=fadd5c00 \
-    --ringwrongcolor=f13459aa --insidewrongcolor=f1345900)
-
-
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
-i3lock "${param[@]}" -i $TMPBG
-rm $TMPBG
+maim \
+  | convert png:- -scale 10% -scale 1000% png:- \
+  | convert png:- "$LOCK" -gravity center -composite -matte png:- \
+  | i3lock -e -i /dev/stdin \
+  --insidecolor=28282899 --ringcolor=EBDBB2FF --line-uses-inside \
+  --keyhlcolor=FB4934FF --bshlcolor=FB4934FF --separatorcolor=282828FF \
+  --insidevercolor=FABD2F99 --insidewrongcolor=FB493499 \
+  --ringvercolor=EBDBB2FF --ringwrongcolor=EBDBB2FF --indpos='x+45:y+2100' \
+  --radius=23 --veriftext='' --wrongtext=''
