9ffcf302da057781f259fb6c08b75e47788f5b4c
Mark Medum Bundgaard <mmedum@gmail.com>
2017-11-29 21:54:25 +0100

    blur more on the lock

--- config/i3/scripts/lock.sh
+++ config/i3/scripts/lock.sh
@@ -9,7 +9,7 @@ param=(--insidecolor=28282899 --ringcolor=EBDBB2FF --line-uses-inside \
   --ringvercolor=EBDBB2FF --ringwrongcolor=EBDBB2FF --indpos='x+50:y+2100' \
   --radius=23 --veriftext='' --wrongtext='')
 
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=10:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
 i3lock "${param[@]}" -i $TMPBG
 
 rm $TMPBG
