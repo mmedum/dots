84886fb723153dfcb401eba357286f2f0fd95ac9
Mark Medum Bundgaard <mmedum@gmail.com>
2020-07-23 21:07:23 +0200

    Adding newest dots from new arch linux installation and cleaning up the last macbook pro dot files

--- arch/config/i3/scripts/lock.sh
+++ arch/config/i3/scripts/lock.sh
@@ -6,10 +6,10 @@ maim $tmpbg
 convert $tmpbg -scale 10% -scale 1000% $tmpbg
 
 i3lock -i $tmpbg \
-  --insidecolor=28282899 --ringcolor=EBDBB2FF --line-uses-inside \
-  --keyhlcolor=FB4934FF --bshlcolor=FB4934FF --separatorcolor=282828FF \
-  --insidevercolor=FABD2F99 --insidewrongcolor=FB493499 \
-  --ringvercolor=EBDBB2FF --ringwrongcolor=EBDBB2FF --indpos='x+40:h-50' \
-  --radius=20 --veriftext='' --wrongtext=''
+    --insidecolor=28282899 --ringcolor=EBDBB2FF --line-uses-inside \
+    --keyhlcolor=FB4934FF --bshlcolor=FB4934FF --separatorcolor=282828FF \
+    --insidevercolor=FABD2F99 --insidewrongcolor=FB493499 \
+    --ringvercolor=EBDBB2FF --ringwrongcolor=EBDBB2FF --indpos='x+40:h-50' \
+    --radius=20 --veriftext='' --wrongtext=''
 
 rm $tmpbg
