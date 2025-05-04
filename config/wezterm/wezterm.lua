a434b94a05188ad181d638224c26fe98d8fbf3de
Mark Medum Bundgaard <mmedum@gmail.com>
2025-05-04 17:31:57 +0200

    Using the new wezterm colorscheme for gruvbox dark material

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,38 +1,14 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.check_for_updates = false
 config.enable_tab_bar = false
 config.adjust_window_size_when_changing_font_size = false
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
+config.quit_when_all_windows_are_closed = true
 config.font = wezterm.font("SauceCodePro Nerd Font")
 config.font_size = 14
-config.color_scheme = "gruvbox_material_dark_medium"
-config.color_schemes = {
-	["gruvbox_material_dark_hard"] = {
-		foreground = "#D4BE98",
-		background = "#1D2021",
-		cursor_bg = "#D4BE98",
-		cursor_border = "#D4BE98",
-		cursor_fg = "#1D2021",
-		selection_bg = "#D4BE98" ,
-		selection_fg = "#3C3836",
-
-		ansi = {"#1d2021","#ea6962","#a9b665","#d8a657", "#7daea3","#d3869b", "#89b482","#d4be98"},
-		brights = {"#eddeb5","#ea6962","#a9b665","#d8a657", "#7daea3","#d3869b", "#89b482","#d4be98"},
-	},
-	["gruvbox_material_dark_medium"] = {
-		foreground = "#D4BE98",
-		background = "#282828",
-		cursor_bg = "#D4BE98",
-		cursor_border = "#D4BE98",
-		cursor_fg = "#282828",
-		selection_bg = "#D4BE98",
-		selection_fg = "#45403d",
-
-		ansi = { "#282828", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
-		brights = { "#eddeb5", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
-	},
-}
+config.color_scheme = "Gruvbox Material (Gogh)"
 
 return config
